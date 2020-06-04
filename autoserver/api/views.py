ea7c9ee33740a9ef76823904415ce6f84b1d2cb9
chengxu1120 <chengxu_1021@126.com>
2020-06-04 15:00:05 +0800

    second commit

--- autoserver/api/views.py
+++ autoserver/api/views.py
@@ -7,5 +7,8 @@ from django.views.decorators.csrf import csrf_exempt
 def get_data(request):
     print(request.GET)
     print(request.POST)
-
+    host = request.POST.get('host')
+    info = request.POST.get('info')
+    print('host:',host)
+    print('info:',info)
     return HttpResponse('成功')
